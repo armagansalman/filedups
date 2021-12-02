b76cdd3311f354baa1d5fb5eba90936c72a2e911
Armağan Salman <armagansalman>
2021-12-02 11:14:39 +0300

    Initial commit.semver_0.0.1

---
+++ src-python/blob (copy 2).py
@@ -0,0 +1,139 @@
+"""
+    <This file is a part of the program armaganymmt-prj-1_name.
+    armaganymmt-prj-1_name processes files from different kinds of
+    locations to find duplicate files.>
+    
+    Copyright (C) <2021>  <Armağan Salman> <gmail,protonmail: armagansalman>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+
+class FilesInfo:
+	def __init__(self, locations, reader_func, size_getter):
+		self.locations = locations
+		self.reader_func = reader_func
+		self.size_getter = size_getter
+	#
+#
+
+class FileIndexer:
+	def __init__(self, files_info_iter):
+		self.data = []
+		# self.g_ref holds (x,y) ; x = file_info idx, y = location idx 
+		# in that  file_info.locations
+		self.g_ref = []
+		
+		for idx, files_info in enumerate(files_info_iter):
+			self.data.append(files_info)
+			for jdx, loc in enumerate(files_info.locations):
+				self.g_ref.append((idx, jdx))
+			#
+		#
+	#
+	
+	def get_location(self, idx):
+		(data_idx, loc_idx) = self.g_ref[idx]
+		locations = self.data[data_idx].locations
+		return locations[loc_idx]
+	#
+	
+	def get_reader(self, idx):
+		data_pos = self.g_ref[idx]
+		return self.data[data_pos[0]].reader_func
+	#
+	
+	def get_size_func(self, idx):
+		data_pos = self.g_ref[idx]
+		return self.data[data_pos[0]].size_getter
+	#
+	
+	def get_idx_count(self):
+		# Total number of locations minus 1.
+		return len(self.g_ref)
+#
+
+def full_reader(file_loc, start_idx, end_idx):
+	return file_loc[:]
+
+def reader_tmp(file_loc, start_idx, end_idx):
+	# TODO(armagan): Use fopen or similar file reading functions.
+	# Read bytes from file starting at idx 'start_idx'
+	# including 'end_idx'.
+	return file_loc[start_idx:end_idx]
+#
+
+def size_getter_tmp(file_loc):
+	# File size in bytes.
+	return len(file_loc)
+#
+
+finf_1 = FilesInfo(["abc", "bac", "cba"], reader_tmp, size_getter_tmp)
+finf_2 = FilesInfo(["dabc", "dbac", "dcba"], reader_tmp, size_getter_tmp)
+finf_3 = FilesInfo(["qdabc", "qdbac", "qdcba"], full_reader, size_getter_tmp)
+
+finfos = [finf_1, finf_2, finf_3]
+
+
+FIDX = FileIndexer(finfos)
+
+print(FIDX.get_idx_count())
+print(FIDX.get_location(8))
+print(FIDX.get_location(5))
+print(FIDX.get_location(0))
+
+for i in range(FIDX.get_idx_count()):
+	location = FIDX.get_location(i)
+	fsize = FIDX.get_size_func(i)(location)
+	fbytes = FIDX.get_reader(i)(location, 0, 2)
+	
+	print((location, fsize, fbytes))
+#
+
+
+
+
+
+
+
+
+
+
+"""
+
+def data_indexer(file_info_iter):
+	all_data = []
+	
+	for elm in file_info_iter:
+		all_data.append(elm)
+	#
+	
+	g_idx = 0
+	g_ref = []
+	for idx, files_info in enumerate(all_data):
+		for loc in files_info.locations:
+			g_ref.append(idx)
+	#
+	print(g_ref)
+	return(g_ref)
+#
+
+
+g_idx = data_indexer(finfos)
+
+print(g_idx[0])
+
+
+"""
---
+++ src-python/util.py
@@ -0,0 +1,103 @@
+"""
+    <This file is a part of the program armaganymmt-prj-1_name.
+    armaganymmt-prj-1_name processes files from different kinds of
+    locations to find duplicate files.>
+    
+    Copyright (C) <2021>  <Armağan Salman> <gmail,protonmail: armagansalman>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+
+import os
+import hashlib
+
+
+def get_fpaths_recursively_from_folder(PATH):
+    rec_files = set()
+    # TODO(armaganslmn): ??? Error handling.
+    if os.path.isfile(PATH):
+        rec_files.add(PATH)
+        return rec_files
+    #
+    
+    for root, dirs, files in os.walk(PATH):
+        for name in files:
+            p = os.path.join(root, name)
+            rec_files.add(os.path.abspath(p))
+        #
+    #
+    return rec_files
+#
+
+
+def get_fpaths_from_path_iter(paths_iter):
+    file_paths = []
+    # TODO(armaganslmn): Handle if input is file.
+    # TODO(armaganslmn): ??? Error handling.
+    for path in paths_iter:
+        file_paths.extend( get_fpaths_recursively_from_folder(path) )
+    #
+    return file_paths
+#
+
+
+def get_file_size_in_bytes(path):
+    statinfo = os.stat(path)
+    return statinfo.st_size
+#
+
+def get_local_file_size(PATH):
+	return get_file_size_in_bytes(PATH)
+#
+
+def local_file_reader(file_path, size_to_read):
+    data = None
+    
+    TODO: fseek
+    
+    with open(file_path, "rb") as in_fobj:
+        data = in_fobj.read(size_to_read)
+    #
+    
+    return data
+#
+
+def file_sha512(file_path, size_to_read):
+    hs = hashlib.sha512()
+    data = None
+    
+    with open(file_path, "rb") as in_fobj:
+        data = in_fobj.read(size_to_read)
+    #
+    
+    hs.update(data)
+    return hs.hexdigest()
+#
+
+"""
+def getSHA256(currentFile, full=False):
+	#Read the 64k at a time, hash the buffer & repeat till finished. 
+	#By default only checksum the first block
+	hasher = hashlib.sha256()
+	with open(currentFile, 'rb') as file:
+		buf = file.read(BLOCKSIZE)
+		while len(buf) > 0:
+			hasher.update(buf)
+			if not full:
+				break
+			buf = file.read(BLOCKSIZE)
+	return hasher.hexdigest()
+"""
